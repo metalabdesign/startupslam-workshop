79f1627f10508af6122f4c25937ca1bd50c640de
Jason Trill <jason@jasontrill.com>
2015-10-01 00:54:37 -0700

    Render loading message from server instaed of app
    So we don't have to stub out libraries or make checks for server/client.

--- config/webpack/server.webpack.config.babel.js
+++ config/webpack/server.webpack.config.babel.js
@@ -1,4 +1,3 @@
-import webpack from 'webpack';
 import nearest from 'find-nearest-file';
 import partial from 'webpack-partial';
 import path from 'path';
@@ -11,9 +10,6 @@ const config = {
   entry: {
     server: path.join(root, 'entry', 'server.entry.js')
   },
-  plugins: [
-    new webpack.NormalModuleReplacementPlugin(/phoenix/, 'phoenix-stub.js')
-  ],
   target: 'node',
   context: root,
   // Output controls the settings for file generation.
--- src/phoenix-stub.js
+++
@@ -1,5 +0,0 @@
-// Phoenix client lib references window, so it crashes on the server
-export class LongPoll {}
-export class Ajax {}
-export class Channel {}
-export class Socket {}
