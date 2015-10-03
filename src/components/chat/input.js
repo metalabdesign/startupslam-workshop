8db779950141bbb7d0997869a1927a5485100fc9
James Phillips <jamesdphillips@gmail.com>
2015-10-03 12:24:22 -0700

    SOLUTION: Send message to server.

--- src/components/chat/input.js
+++ src/components/chat/input.js
@@ -20,6 +20,7 @@ export default class Input extends Component {
     // On enter, submit the chat msg and clear the field
     if (ev.keyCode === 13) {
       this.setState({value: ''});
+      this.props.messageSend(this.state.value);
     }
   }
 
