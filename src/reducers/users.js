78d272889a8d40829576f5d2f29b786806dc3b2f
James Phillips <jamesdphillips@gmail.com>
2015-10-02 22:24:29 -0700

    Update users on presence update

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,19 +1,14 @@
-import { map, indexBy } from 'lodash';
+import { indexBy } from 'lodash';
 import {
-  MESSAGES_FETCH,
   USERS_FETCH,
   USERS_UPDATE_PRESENCE,
 } from '../action-types';
 
 export default function users(state = {}, action) {
   switch (action.type) {
-  case MESSAGES_FETCH:
-    const authors = indexBy(map(action.payload, msg => msg.user), 'id');
-    return {...state, ...authors};
   case USERS_UPDATE_PRESENCE:
-    const data = action.payload;
-    const user = state[action.payload.id];
-    return user ? {...state, [user.id]: {...user, online: data.online}} : state;
+    const user = action.payload;
+    return {...state, [user.id]: user};
   case USERS_FETCH:
     const users = indexBy(action.payload, 'id');
     return {...state, ...users};
