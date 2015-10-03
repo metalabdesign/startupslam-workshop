600e9cde7903cec7c12ec32910cd346a5bb196d2
James Phillips <jamesdphillips@gmail.com>
2015-10-02 21:35:01 -0700

    Fetch all users on boot

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,18 +1,22 @@
-import { chain } from 'lodash';
-import { MESSAGES_FETCH, USERS_UPDATE_PRESENCE } from '../action-types';
+import { map, indexBy } from 'lodash';
+import {
+  MESSAGES_FETCH,
+  USERS_FETCH,
+  USERS_UPDATE_PRESENCE,
+} from '../action-types';
 
 export default function users(state = {}, action) {
   switch (action.type) {
   case MESSAGES_FETCH:
-    const authors = chain(action.payload)
-      .map(msg => msg.user)
-      .indexBy('id')
-      .value();
+    const authors = indexBy(map(action.payload, msg => msg.user), 'id');
     return {...state, ...authors};
   case USERS_UPDATE_PRESENCE:
     const data = action.payload;
     const user = state[action.payload.id];
     return user ? {...state, [user.id]: {...user, online: data.online}} : state;
+  case USERS_FETCH:
+    const users = indexBy(action.payload, 'id');
+    return {...state, ...users};
   default:
     return state;
   }
